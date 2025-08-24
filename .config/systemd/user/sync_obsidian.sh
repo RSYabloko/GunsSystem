9fb54676d797ae3ca7e6ba8140f6cd49930fdef8
RSYabloko <RSYabloko@gmail.com>
2025-08-24 23:59:20 +0300

    Repair Sync_Obsidian

--- .config/systemd/user/sync_obsidian.sh
+++ .config/systemd/user/sync_obsidian.sh
@@ -4,9 +4,11 @@ source ~/.config/systemd/user/.secret.sh
 
 WATCH_DIR="/home/Naga/Obsidian"        # Локальная папка Obsidian
 
+rsync -az --delete -e "ssh -i $REMOTE_KEY -p $REMOTE_PORT" "$WATCH_DIR/" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
+
 inotifywait -m -r -e modify,create,delete,move "$WATCH_DIR" --format '%w%f' |
 while read FILE
 do
   echo "Detected change in $FILE"
-  rsync -az --delete -e "ssh -p $REMOTE_PORT" "$WATCH_DIR/" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
+  rsync -az --delete -e "ssh -i $REMOTE_KEY -p $REMOTE_PORT" "$WATCH_DIR/" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 done
